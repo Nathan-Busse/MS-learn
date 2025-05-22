96508487cd73f1ce9f2a8aa4cf7543519d92ebc4
Nathan <bussenathan@icloud.com>
2025-05-22 16:52:25 +0200

    patched some syntax errors

--- course-list/D-700/Fabric_Data_Engineer/python_scripts/Module4/Exeercise1.py
+++ course-list/D-700/Fabric_Data_Engineer/python_scripts/Module4/Exeercise1.py
@@ -1,20 +1,20 @@
- from pyspark.sql.types import *
+from pyspark.sql.types import *
     
- # Create the schema for the table
- orderSchema = StructType([
-     StructField("SalesOrderNumber", StringType()),
-     StructField("SalesOrderLineNumber", IntegerType()),
-     StructField("OrderDate", DateType()),
-     StructField("CustomerName", StringType()),
-     StructField("Email", StringType()),
-     StructField("Item", StringType()),
-     StructField("Quantity", IntegerType()),
-     StructField("UnitPrice", FloatType()),
-     StructField("Tax", FloatType())
-     ])
+# Create the schema for the table
+orderSchema = StructType([
+    StructField("SalesOrderNumber", StringType()),
+    StructField("SalesOrderLineNumber", IntegerType()),
+    StructField("OrderDate", DateType()),
+    StructField("CustomerName", StringType()),
+    StructField("Email", StringType()),
+    StructField("Item", StringType()),
+    StructField("Quantity", IntegerType()),
+    StructField("UnitPrice", FloatType()),
+    StructField("Tax", FloatType())
+])
     
- # Import all files from bronze folder of lakehouse
- df = spark.read.format("csv").option("header", "true").schema(orderSchema).load("Files/bronze/*.csv")
-    
- # Display the first 10 rows of the dataframe to preview your data
- display(df.head(10))
+# Import all files from bronze folder of lakehouse
+df = spark.read.format("csv").option("header", "true").schema(orderSchema).load("Files/bronze/*.csv")
+
+# Display the first 10 rows of the dataframe to preview your data
+display(df.head(10))
